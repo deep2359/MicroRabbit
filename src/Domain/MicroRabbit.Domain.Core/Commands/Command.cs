2dd369a5da6baef32d9b35f987e51fd41863d56c
deep2359 <deep2359@gmail.com>
2022-03-08 13:59:07 +0530

    Implimented Interfaces and Classes for Domain.Core

---
+++ src/Domain/MicroRabbit.Domain.Core/Commands/Command.cs
@@ -0,0 +1,15 @@
+﻿using MicroRabbit.Domain.Core.Events;
+using System;
+
+namespace MicroRabbit.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTimeOffset TimeOffset { get; protected set; }
+
+        protected Command()
+        {
+            TimeOffset = DateTimeOffset.Now;
+        }
+    }
+}
---
+++ src/Domain/MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Event
+    {
+        public DateTimeOffset TimeOffset { get; protected set; }
+
+        protected Event()
+        {
+            TimeOffset = DateTimeOffset.Now;
+        }
+    }
+}
